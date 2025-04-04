716c89ac3a58a5020b0fef930bc7138c9429549a
Madhuri076 <madhurimandala076@gmail.com>
2025-04-04 18:41:25 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello
+  {
+    public static void main(String args[])
+    {
+      System.out.println("Welcome to Devops Lab");
+      System.out.println("04:04:2025, 06:40PM");
+    }
+  }
